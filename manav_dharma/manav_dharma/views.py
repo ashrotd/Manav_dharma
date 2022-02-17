7a2a229b81be54cabc525dd4853beaafac06bee3
ashrotd <63250878+ashrotd@users.noreply.github.com>
2022-02-17 23:19:39 +0545

    video

--- manav_dharma/manav_dharma/views.py
+++ manav_dharma/manav_dharma/views.py
@@ -3,11 +3,12 @@ from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import BlogModel
+from video.models import VideoSatsang
 
 def home(request):
     blog = BlogModel.objects.all().order_by('-created_date')[:3]
-    
-    context = {'blog':blog}
+    videos = VideoSatsang.objects.all().order_by('-created_date')[:3]
+    context = {'blog':blog,'videos':videos}
     return render(request,'home.html',context)
 
 def about(request):
