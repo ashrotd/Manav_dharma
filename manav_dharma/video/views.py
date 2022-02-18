5d0e55ac5508dca2e4a69f7c7c08ad748ecec9e4
ashrotd <63250878+ashrotd@users.noreply.github.com>
2022-02-18 20:29:49 +0545

    Video Satsang

--- manav_dharma/video/views.py
+++ manav_dharma/video/views.py
@@ -2,9 +2,13 @@ from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import VideoSatsang
+from django.core.paginator import Paginator
 
 # Create your views here.
 def video(request):
     videos = VideoSatsang.objects.all()
-    context = {'video':videos}
+    paginator = Paginator(videos,9)
+    page = request.GET.get('page')
+    paged_videos = paginator.get_page(page)
+    context = {'video':paged_videos}
     return render(request,'video.html',context)
