764a8677a03f36cdfe92af86d80c5baab7cff09b
ashrotd <63250878+ashrotd@users.noreply.github.com>
2022-02-25 12:02:10 +0545

    Bhajans added

--- manav_dharma/audio/models.py
+++ manav_dharma/audio/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class BhajansAudio(models.Model):
+    file = models.FileField(upload_to='audio/')
+    created_date = models.DateTimeField(auto_now_add=True)
+    
--- manav_dharma/audio/views.py
+++ manav_dharma/audio/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import BhajansAudio
+from django.core.paginator import Paginator
 # Create your views here.
 def audio(request):
-    return render(request,'audio.html')
+    bhajans = BhajansAudio.objects.all()
+    paginator = Paginator(bhajans,9)
+    page = request.GET.get('page')
+    paged_bhajans = paginator.get_page(page)
+    context = {'bhajans':paged_bhajans}
+    return render(request,'audio.html',context)
