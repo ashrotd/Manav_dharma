150f26b572c106a8c2b9c4130749893ee29194d1
ashrotd <63250878+ashrotd@users.noreply.github.com>
2022-02-27 15:33:57 +0545

    Blog Update

--- manav_dharma/blog/views.py
+++ manav_dharma/blog/views.py
@@ -21,6 +21,7 @@ def blog_detail(request, blog_slug):
         single_blog = BlogModel.objects.get(slug=blog_slug)
     except Exception as e:
         raise e
-    recent_blog = BlogModel.objects.all().order_by('-created_date')[:3]
-    context = {'single_blog':single_blog, 'recent_blog':recent_blog}
+    recent_blog = BlogModel.objects.all().order_by('-created_date')[:5]
+    day = single_blog.created_date.strftime("%b")
+    context = {'single_blog':single_blog, 'recent_blog':recent_blog,'day':day}
     return render(request,'blog_detail.html',context)
