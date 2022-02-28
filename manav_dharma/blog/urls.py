1a86baff650ebeca15511465d5e6568788033f4d
ashrotd <63250878+ashrotd@users.noreply.github.com>
2022-02-28 10:58:45 +0545

    Slug Changed

--- manav_dharma/blog/urls.py
+++ manav_dharma/blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.blog, name='blog'),
-    path('/<slug:blog_slug>/', views.blog_detail, name='blog_detail')
+    path('<slug:blog_slug>/', views.blog_detail, name='blog_detail')
 ]
 
