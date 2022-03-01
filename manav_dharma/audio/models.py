2633f886dc04fa5e4a6c59916dcc15a67597932e
ashrotd <63250878+ashrotd@users.noreply.github.com>
2022-03-01 11:46:14 +0545

    Name in Audio

--- manav_dharma/audio/models.py
+++ manav_dharma/audio/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 class BhajansAudio(models.Model):
+    name = models.CharField(max_length=50)
     file = models.FileField(upload_to='audio/')
     created_date = models.DateTimeField(auto_now_add=True)
     
+    def __str__(self):
+        return self.name
